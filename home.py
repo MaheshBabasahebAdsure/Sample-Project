0ada855539c42068c4b12a680af9d5ec6efe6803
MaheshBabasahebAdsure <92207158+MaheshBabasahebAdsure@users.noreply.github.com>
2021-10-09 02:41:08 -0700

    pycharm pr

---
+++ home.py
@@ -0,0 +1,30 @@
+#print('welcome')
+
+#print(a)
+#print(type(a))
+
+#a=10
+#b=20
+#c=a+b
+#print("Addition=",c)
+
+#user define
+#a=int(input("Enter a"))
+#b=int(input("Enter b"))
+
+#c=a+b
+#print("Addition=",c)
+
+print("Enter Marks 3 Subjects: ")
+m1= int(input())
+m2= int(input())
+m3= int(input())
+
+sum = m1+m2+m3
+print("total marks=",sum)
+avg = sum/3
+print("Average=",avg)
+perc = (sum/300)*100
+perc = (sum/300)*100
+
+print("Percentage Mark = ",perc)
